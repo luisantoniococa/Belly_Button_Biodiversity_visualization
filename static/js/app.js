fa482def97ae4c08606aaa6397a4cf29f4c46495
luisantoniococa <luicocurd@gmail.com>
2020-02-14 13:10:00 -0600

    we generated one of the graphs and modified the function that retrieves the value from the samples

--- static/js/app.js
+++ static/js/app.js
@@ -34,9 +34,39 @@ function buildCharts(sample) {
       // newSampleObj.map((x) => newSampleObj2.push({ 1:`${x.sample_values}`}))
       delete newSampleObj.id
       // newSampleObj.forEach((x) => newSampleObj2.push({"sample_values": i.sample_values}))
+
+
+      // we created an array of objects with the individual data since it will be easier to organize and aggregate the data.
+      for(var i = 0; i<newSampleObj.sample_values.length;i++){
+        newSampleObj2.push({"sample_values": newSampleObj.sample_values[i],"otu_ids": newSampleObj.otu_ids[i], 'otu_labels': newSampleObj.otu_labels[i] })
+      };
+      console.log(newSampleObj2);
+      var top10founded = newSampleObj2.sort((a,b)=> b.sample_values - a.sample_values);
+      console.log(top10founded);
+      top10founded = top10founded.slice(0,10);
+      console.log(top10founded);
+      top10founded = top10founded.reverse();
+      console.log(top10founded);
+
+      var trace1 = {
+        x: top10founded.map(object => object.sample_values),
+        y: top10founded.map(object => `OTU ${object.otu_ids}`),
+        text: top10founded.map(object => object.otu_labels),
+        // name: "",
+        type: "bar",
+        orientation: "h"
+      };
       
-      console.log(newSampleObj.sample_values[0])
+      // data
+      var data = [trace1];
       
+      // Apply the group bar mode to the layout
+      var layout = {
+        title: "Top 10 OTUs Found in Individual",
+      };
+      
+      // Render the plot to the div tag with id "plot"
+      Plotly.newPlot("bar", data, layout);
     })
 
 };
@@ -79,7 +109,7 @@ function init() {
         
       });
 
-      function optionChanged(newSample) {
+      function thi(newSample) {
         // Fetch new data each time a new sample is selected
         buildCharts(newSample);
         buildMetadata(newSample);
